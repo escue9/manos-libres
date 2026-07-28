c9ca6f5669a9dec7a143af73cd2acde9d9b03284
Juan Martín Aróztegui <escue9@gmail.com>
2026-07-28 20:36:36 -0300

    Iconos de atajos e id en el manifest

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Subí CACHE_VERSION en cada deploy o los usuarios quedan con la versión vieja.
  */
 
-const CACHE_VERSION = 'cocina-cic-v1';
+const CACHE_VERSION = 'cocina-cic-v2';
 
 const SHELL = [
   './',
@@ -31,6 +31,9 @@ const SHELL = [
   './assets/logo-192.png',
   './assets/logo-mark.png',
   './assets/favicon.png',
+  './assets/shortcut-venta.png',
+  './assets/shortcut-pedido.png',
+  './assets/shortcut-jornada.png',
 ];
 
 self.addEventListener('install', (e) => {
